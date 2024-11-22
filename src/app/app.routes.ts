6ef360393fdcf77ac22d76b4f9c46dcd8baf0cdb
Diego C. <delcaos12@gmail.com>
2024-11-22 18:03:07 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
+        pathMatch: 'full'
+    },
+    { 
+        path: 'projects', 
+        loadChildren: () => import('./projects/projects.routes').then(m => m.PROJECTS_ROUTES)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
